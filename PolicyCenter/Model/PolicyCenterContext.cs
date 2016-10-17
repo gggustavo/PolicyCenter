bc0581757d32405f91f738a1d6976263d4e991b0
Gustavo Garcia <garciagustavoariel@hotmail.com>
2016-10-16 21:15:27 -0300

    first commit

---
+++ PolicyCenter/Model/PolicyCenterContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Model
+{
+    public class PolicyCenterContext : DbContext
+    {
+        public PolicyCenterContext() : base("name=Default")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+
+        }
+
+        public Marca Marca { get; set; }
+        public Modelo Modelo { get; set; }
+        public Version Version { get; set; }
+        public Anio Anio { get; set; }
+
+
+    }
+}
