24047698b00b9e410930958b7eb743e8292d67d5
German Andino <andinog@hotmail.com>
2016-10-21 15:04:28 -0300

    hello world

--- PolicyCenter/Model/Vehiculo.cs
+++ PolicyCenter/Model/Vehiculo.cs
@@ -1,10 +1,12 @@
-﻿namespace Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
 {
     public class Vehiculo
-    {        
+    {
         public int IdVehiculo { get; set; }
-        public int IdMarca { get; set; }
-        public Marca Marca { get; set; }
+        public int IdVersion { get; set; }
+        public Version Version { get; set; }
         public string Anio { get; set; }
         public string Patente { get; set; }
         public string Color { get; set; }
