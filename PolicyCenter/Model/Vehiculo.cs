460d2391868e8117b7963819dc63aa28c4d86c44
gggustavo <garciagustavoariel@hotmail.com>
2016-10-24 22:26:04 -0300

    change vehiculo

--- PolicyCenter/Model/Vehiculo.cs
+++ PolicyCenter/Model/Vehiculo.cs
@@ -1,12 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Model
+﻿namespace Model
 {
     public class Vehiculo
     {
         public int IdVehiculo { get; set; }
-        public int IdVersion { get; set; }
-        public Version Version { get; set; }
+        public int IdMarca { get; set; }
+        public Marca Marca { get; set; }
         public string Anio { get; set; }
         public string Patente { get; set; }
         public string Color { get; set; }
